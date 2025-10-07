7bfa73c617de96f442b288e4499d1bc3b7021279
bilonjea <j.bilong@gmail.com>
2025-10-07 17:19:37 +0200

    udapet config for authentication

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@
 # you can also use the weekly version (more recent but less stable)
 # like jenkins/jenkins:weekly-jdk21 or jenkins/jenkins:latest-jdk21 or jenkins/jenkins:jdk21
 
-FROM jenkins/jenkins:2.479.2-lts-jdk21
+FROM jenkins/jenkins:2.516.3-lts-jdk21
 
 USER root
 ENV DEBIAN_FRONTEND=noninteractive
@@ -32,5 +32,15 @@ USER jenkins
 # Plugins Jenkins (installés au build)
 ARG PLUGINS_FILE=jenkins-plugins/plugins.txt
 COPY --chown=jenkins:jenkins ${PLUGINS_FILE} /usr/share/jenkins/ref/plugins.txt
+
+# Utiliser l’update center OFFICIEL (petit JSON)…
+ENV JENKINS_UC=https://updates.jenkins.io \
+    JENKINS_UC_EXPERIMENTAL=https://updates.jenkins.io/experimental
+
+# …mais télécharger les .hpi depuis le miroir (gros fichiers)
+ENV JENKINS_UC_DOWNLOAD=https://mirrors.tuna.tsinghua.edu.cn/jenkins
+# (optionnel) verbosité
+ENV JENKINS_PLUGIN_CLI_VERBOSE=true
+
 RUN jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
 
--- casc_jobs/cargo-tms.groovy
+++ casc_jobs/cargo-tms.groovy
@@ -1,31 +1,90 @@
-multibranchPipelineJob('cargo-tms') {
-  branchSources {
-    branchSource {
-      source {
-        github {
-          id('gh-cargo')
-          repoOwner('bilonjea')
-          repository('cargo-tms')
-          configuredByUrl(false)
-          // Laisse vide si repo public ; sinon mets "github-pat"
-          credentialsId('github-pat')
-          buildForkPRHead(true)
-          buildOriginBranch(true)
-          buildOriginPRHead(true)
+multibranchPipelineJob("cargo-tms") {
+    displayName("Cargo TMS Pipeline")
+    description("Multibranch pipeline for cargo-tms repository")
+    
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('gh-cargo')  // ID unique pour cette source
+                    repositoryUrl('https://github.com/bilonjea/cargo-tms')
+                    configuredByUrl(true)
+                    // credentialsId('gh-pat') // Décommentez si dépôt privé
+                    traits {
+                        gitHubBranchDiscovery {
+                            strategyId(1) // Découvre seulement les branches
+                        }
+                        gitHubPullRequestDiscovery {
+                            strategyId(1) // Découvre les PR depuis la branche source
+                        }
+                        gitHubForkDiscovery {
+                            strategyId(1) // Découvre les PR depuis les forks
+                            trust('Permission')
+                        }
+                        cleanBeforeCheckoutTrait() // Nettoie le workspace avant checkout
+                        cleanAfterCheckoutTrait() // Nettoie après checkout
+                        cloneOptionTrait {
+                            extension {
+                                shallow(true) // Clone shallow pour plus de rapidité
+                                depth(1)
+                                timeout(10)
+                                noTags(true)
+                            }
+                        }
+                    }
+                }
+            }
+            strategy {
+                defaultBranchPropertyStrategy {
+                    props {
+                        // Pas de propriétés supplémentaires pour l'instant
+                    }
+                }
+            }
         }
-      }
-      strategy {
-        defaultBranchPropertyStrategy { props { } }
-      }
     }
-  }
-  factory {
-    workflowBranchProjectFactory { scriptPath('Jenkinsfile') }
-  }
-  orphanedItemStrategy {
-    discardOldItems { numToKeep(10) }
-  }
-  triggers {
-    periodicFolderTrigger { interval('1d') } // rescan quotidien
-  }
+    
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile') // Chemin vers le Jenkinsfile dans le repo
+        }
+    }
+    
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10) // Garde les 10 dernières builds
+            daysToKeep(-1) // Pas de limite en jours
+        }
+    }
+    
+    triggers {
+        periodicFolderTrigger {
+            interval('1d') // Scan quotidien du repository
+        }
+    }
+    
+    properties {
+        folderCredentialsProperty {
+            domainCredentials {
+                domainCredentials {
+                    domain {
+                        name('')
+                        description('')
+                    }
+                    credentials {
+                        // Ajoutez ici les credentials si nécessaires
+                    }
+                }
+            }
+        }
+    }
+    
+    configure { project ->
+        project / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits' {
+            'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+                includes('*') // Inclut toutes les branches
+                excludes('')  // N'exclut aucune branche
+            }
+        }
+    }
 }
