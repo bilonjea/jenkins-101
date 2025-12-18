fe9e4214384eca88715fc69eb4835412e48a1e55
bilonjea <j.bilong@gmail.com>
2025-12-19 00:42:38 +0100

    test 1

---
+++ casc_jobs/cargo-tms-pipeline.groovy
@@ -0,0 +1,19 @@
+pipelineJob('cargo-tms-pipeline') {
+    displayName('Cargo TMS Pipeline')
+    description('Pipeline simple pour le projet Cargo TMS')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/bilonjea/cargo-tms.git')
+                        // credentials('gh-pat') // Décommente si privé
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('Jenkinsfile') // Le Jenkinsfile doit être à la racine du repo
+        }
+    }
+}
