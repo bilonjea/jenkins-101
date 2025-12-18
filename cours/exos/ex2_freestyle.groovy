ee9caf2f520cc000708d8711b444b65d098348cb
jnbilong <jnbilong@partner.oney.fr>
2025-12-19 00:05:27 +0100

    test

--- cours/exos/ex2_freestyle.groovy
+++ cours/exos/ex2_freestyle.groovy
@@ -2,15 +2,29 @@ job('cargo-tms-freestyle') {
     displayName('Cargo TMS Freestyle')
     description('Job freestyle pour build contracts-api')
 
+    // Supprime le workspace avant le build
+    wrappers {
+        preBuildCleanup()
+    }
+
+    // Clone du repo public
     scm {
         git('https://github.com/bilonjea/cargo-tms.git', 'main')
     }
 
+    // Build contracts-api
     steps {
         shell('''
             echo "La variable BUILD_NUMBER vaut : $BUILD_NUMBER"
             cd contracts-api/
-            mvn compile
+            mvn clean package
         ''')
+    }
+
+    // Archive le jar généré
+    publishers {
+        archiveArtifacts {
+            pattern('contracts-api/target/*.jar')
+        }
     }
 }
