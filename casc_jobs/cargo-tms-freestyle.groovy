2f9dd565acda7730f63c9a27e306de83e6b711c5
bilonjea <j.bilong@gmail.com>
2025-12-19 00:07:03 +0100

    test

---
+++ casc_jobs/cargo-tms-freestyle.groovy
@@ -0,0 +1,16 @@
+job('cargo-tms-freestyle') {
+    displayName('Cargo TMS Freestyle')
+    description('Job freestyle pour build contracts-api')
+
+    scm {
+        git('https://github.com/bilonjea/cargo-tms', 'main')
+    }
+
+    steps {
+        shell('''
+            echo "La variable BUILD_NUMBER vaut : $BUILD_NUMBER"
+            cd contracts-api/
+            mvn compile
+        ''')
+    }
+}
---
+++ cours/exos/ex3_multibranch.groovy
@@ -0,0 +1,50 @@
+multibranchPipelineJob("cargo-tms-multibranch") {
+    displayName("Cargo TMS Pipeline")
+    description("Multibranch pipeline for cargo-tms repository")
+
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('gh-cargo')
+                    repositoryUrl('https://github.com/bilonjea/cargo-tms')
+                    configuredByUrl(true)
+                    // credentialsId('gh-pat') // Décommentez si privé
+                    traits {
+                        gitHubBranchDiscovery { strategyId(1) }
+                        cleanBeforeCheckoutTrait()
+                        cleanAfterCheckoutTrait()
+                        cloneOptionTrait {
+                            extension {
+                                shallow(true)
+                                depth(1)
+                                timeout(10)
+                                noTags(true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+            daysToKeep(-1)
+        }
+    }
+
+    triggers {
+        // Scan toutes les 2 minutes (pour simuler un déclenchement rapide)
+        periodicFolderTrigger {
+            interval('2m')
+        }
+    }
+}
